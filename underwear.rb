17836d2e544a3cc15dd818a10e8ae785f75241aa
Hennie Bruurmijn <hlm.bruurmijn@gmail.com>
2022-05-08 10:38:53 +0700

    Rewrite code to accept full sentences and upcase
    Old code is preserved for reference as quite a bit had to be changed.

--- underwear.rb
+++ underwear.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 def substrings(text, dict)
-  dict.each_with_object(Hash.new(0)) do |word, result|
-    result[word] += 1 if text.include?(word)
+  text = text.downcase.split(' ')
+  count_list = text.each_with_object(Hash.new(0)) do |word, output|
+    dict.each { |entry| output[entry] += 1 if word.include?(entry) }
   end
+  p count_list
 end
 
 dictionary = %w[below down go going horn how howdy it i low own part partner sit]
@@ -11,9 +13,20 @@ dictionary = %w[below down go going horn how howdy it i low own part partner sit
 sub_word = 'below'
 sub_sentence = "Howdy partner, sit down! How's it going?"
 
-p substrings(sub_word, dictionary)
+substrings(sub_word, dictionary)
 
-p substrings(sub_sentence, dictionary)
+substrings(sub_sentence, dictionary)
 
 # expected outcome
 # { "below" => 1, "low" => 1 }
+
+# def substrings(text, dict)
+#   #text = text.split(" ")
+#   p text
+#   dict.reduce(Hash.new(0)) do |result, word|
+#     if text.include?(word)
+#       result[word] += 1
+#     end
+#     result
+#   end
+# end
