6d0a7200ea7e6c37f7a52e9a9ef110c7e3c6ff21
Hennie Bruurmijn <hlm.bruurmijn@gmail.com>
2022-05-08 07:27:59 +0700

    Fix formatting and add variable for method call

--- underwear.rb
+++ underwear.rb
@@ -1,18 +1,19 @@
+# frozen_string_literal: true
+
 def substrings(text, dict)
-
-  dict.reduce(Hash.new(0)) do |result, word|
-    if text.include?(word)
-      result[word] += 1
-    end
-
-    result
+  dict.each_with_object(Hash.new(0)) do |word, result|
+    result[word] += 1 if text.include?(word)
   end
-
 end
 
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+dictionary = %w[below down go going horn how howdy it i low own part partner sit]
 
-p substrings("below", dictionary)
+sub_word = 'below'
+sub_sentence = "Howdy partner, sit down! How's it going?"
+
+p substrings(sub_word, dictionary)
+
+p substrings(sub_sentence, dictionary)
 
 # expected outcome
 # { "below" => 1, "low" => 1 }
